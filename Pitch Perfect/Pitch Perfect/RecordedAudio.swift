def99bc40fcc307d50be84b5042705755dee16cc
appsism <appsism@yahoo.com>
2015-04-06 17:16:44 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Ganesh Sadasivan on 4/6/15.
+//  Copyright (c) 2015 Ganesh Sadasivan. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
